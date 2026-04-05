3297d23c3da800c9a77c9c0e2a18c4b6e6ec53d7
Hasan Ali <hassan.ali@stargallery.ae>
2026-04-05 14:08:50 +0400

    ingest: langchain integration

--- ingest/policies.py
+++ ingest/policies.py
@@ -1,3 +1,11 @@
+import requests
+import os
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters.character import CharacterTextSplitter
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_chroma import Chroma
+import re
+import copy
 
 def process_policy(file_name: str, s3_url: str):
     print(f"Ingesting file: {file_name}")
